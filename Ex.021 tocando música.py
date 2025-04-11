20a014c98f71d67efe789429cc9b657786f6a746
sofffb <sofia1708_@hotmail.com>
2025-04-11 20:43:53 -0300

    atualização informações

--- Ex.021 tocando música.py
+++ Ex.021 tocando música.py
@@ -1,3 +1,7 @@
+# como reproduzir um arquivo MP3
+# primeiro ter um aquivo com um nome simples, pequeno, sem caracteres
+# adicionar o arquivo ao pycharm
+
 import pygame
 pygame.init()
 pygame.mixer.music.load('music.mp3')
---
+++ manipulando cadeias de texto.py
@@ -0,0 +1,16 @@
+# prática de manipulação de texto
+
+frase = 'Curso em Vídeo Python'
+print (frase)
+# a quarta letra da frase:
+print (frase[3])
+# fatiar da quarta letra até a décima terceira:
+print (frase[3:13])
+# do início até o 13:
+print (frase[:13])
+# da décima terceira letra até o final:
+print(frase[13:])
+# da 1 até 15
+print(frase[1:15])
+# ler a frase e pular de duas em duas letras:
+print(frase[::2])
