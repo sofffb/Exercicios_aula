6f1b0cccbfbaaa7ed663162ad4c864ca6edcd78c
sofffb <sofia1708_@hotmail.com>
2025-04-18 01:28:40 -0300

    atualização exercícios

--- Ex.022 formatando uma string.py
+++ Ex.022 formatando uma string.py
@@ -1,50 +1,55 @@
 # Programa que leia o nome de uma pessoa e mostre:
 import emoji
 print(emoji.emojize(':beaming_face_with_smiling_eyes:'))
-nome = input(str('Ola! Digite o seu nome: '))
+nome = input(str( '  \n  Ola! Digite somente seu primeiro nome: '))
 
 # O nome com todas as letras em MAIÚSCULO:
-print('  -> O nome todo em letras maiúsculas fica:')
-print(nome.upper())
+
+print(' \n -> O nome todo em letras maiúsculas fica:')
+print(nome.upper() , ' \n ')
+
 # O nome com todas as letras em MINÚSCULO:
 print('  -> O nome todo em letras minúsculas fica:')
-print(nome.lower())
+print(nome.lower() , ' \n ')
 
 # Quantas letras ao total:
 print('  -> Quantos caracteres tem neste nome: ')
-print(len(nome))
+print(len(nome) , ' \n ')
+
 # Quantas letras ao total SEM CONSIDERAR ESPAÇOS:
 nomesemespaco = nome.replace(' ', '')
 print('  -> O nome sem espaçamentos: ')
-print(nomesemespaco)
+print(nomesemespaco , ' \n ')
 
 print('  -> Quantos caracteres tem o nome, sem contar os espaços:')
-print(len(nomesemespaco))
+print(len(nomesemespaco) , ' \n ')
 
 print('''  -> Para conseguir contabilizar os caracteres, primeiro divido para usar a função len 
 - Nome dividido: ''')
 nomediv = nome.split()
 print(nomediv)
 
-print('  -> Quantas letras tem o primeiro nome e o segundo nome, respectivamente:')
+print('  \n  -> Quantas letras tem o primeiro nome e o segundo nome, respectivamente:')
 print(len(nomediv[0]))
 print(len(nomediv[1]))
 
-print('  -> Mostre o primeiro nome e segundo nome separadamente:')
+print('  \n  -> Mostre o primeiro nome e segundo nome separadamente:')
 print(nomediv[0])
 print(nomediv[1])
 
-nomecasada = input('  -> Digite o sobrenome de casado(a): ')
+nomecasada = input(str('  \n  -> Digite o sobrenome de casado(a): '))
+
 
 # incluir  na lista
-print('  -> Adicionando o sobrenome:')
+
+print('  \n  -> Adicionando o sobrenome:')
 nomediv.append(nomecasada)
-print(nomediv)
+print(nomediv , ' \n ')
 
 # remover da lista - em standby
 print('  -> Removendo o sobrenome:')
 nomediv.pop(2)
-print(nomediv)
-print('  ')
+print(nomediv , ' \n ')
+
 print(' Fim do programa! ')
 print(emoji.emojize(':beaming_face_with_smiling_eyes:'))
--- Ex.023 unidade dezena centena milhar.py
+++ Ex.023 unidade dezena centena milhar.py
@@ -1,10 +1,11 @@
 # Programa que leia um número entre 0 à 9999 e mostre na tela cada um dos dígitos separados:
 # tente fazer como uma STRING e MATEMATICAMENTE
 
-num = str(input('Digite um número qualquer entre 0 à 9999 ->  '))
-
-print(f'Unidade: {num[3]}')
-print(f'Dezena: {num[2]}')
-print(f'Centena: {num[1]}')
-print(f'Milhar: {num[0]}')
+num = int(input('Digite um número qualquer entre 0 à 9999 ->  '))
+# transformando número em texto
+n = str(num)
+print(f'Unidade: {n[3]}')
+print(f'Dezena: {n[2]}')
+print(f'Centena: {n[1]}')
+print(f'Milhar: {n[0]}')
 
