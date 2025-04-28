d595c89fb9f8fee72b7f602088bfe5d8bad4f15c
sofffb <sofia1708_@hotmail.com>
2025-04-28 11:03:53 -0300

    Update Ex.024 find SANTO na string.py

--- Ex.024 find SANTO na string.py
+++ Ex.024 find SANTO na string.py
@@ -1,8 +1,10 @@
 # Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com "SANTO"
 
-cidade = input('Digite aqui o nome da sua cidade: ')
-divida = cidade.split()
-print(divida)
+cidade = str(input('Digite aqui o nome da sua cidade: ')) .strip()
+
+cid = cidade.capitalize()
+divida = cid.split()
 separado = (divida[0])
+print(divida)
 print('A primeira palavra da cidade começa com "Santo"?: ')
 print('Santo' in separado)
