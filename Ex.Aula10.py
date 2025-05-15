75f86a90f61ff6b5390fb0704e575c1cf794480d
sofffb <sofia1708_@hotmail.com>
2025-05-15 12:02:36 -0300

    exercício exemplos aula condições

---
+++ Ex.Aula10.py
@@ -0,0 +1,6 @@
+# Aprendendo condições
+# programa que leia o tempo de um carro e diga se é novo ou velho
+
+tempo = int(input('Quantos anos tem o seu carro?: '))
+print('Seu carro é novo!' if tempo<=3 else 'Seu carro é velho.')
+print('Fim do programa!')
