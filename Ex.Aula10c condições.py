afba4161b515d5c566b0d66ed7864f9a7aeb478d
sofffb <sofia1708_@hotmail.com>
2025-05-15 13:16:31 -0300

    exercício calcular média com condição

---
+++ Ex.Aula10c condições.py
@@ -0,0 +1,10 @@
+
+print('Vamos calcular sua média e saber se foi aprovado(a)!')
+aluno = str(input('Digite seu nome: ')) .title()
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1 + n2) / 2
+print(f'{aluno}, sua média foi: {m}')
+print('PARABÉNS você passou! 😄' if m >=6 else 'Você está na recuperação 😥')
+
+
